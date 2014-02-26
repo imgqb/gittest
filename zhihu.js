41f9c1b7a5e7444396b42123843e3c80d6555357
imgqb <iamgqb@gmail.com>
2014-02-26 20:03:37 +0800

    每个回答统计为单个对象呈现，全部塞入数组中输出

--- zhihu.js
+++ zhihu.js
@@ -1,126 +1,149 @@
-var mycookie = '_xsrf=31db63af8fdb49118ef2246eb0ed53a2; q_c1=591380ddf13d4e32969aafc159a80535|1391344146000|1388490028000; q_c0="ODFkYTJlYWQwMjgyOGE1MzBjODFiNmIzN2UwZWE5MGJ8OUYxbTlyYm9nZkVxeEtjZg==|1391344169|ae881221835612588a60df7a64d5b18d610dd465"; _ga=GA1.2.188652600.1391760811; zata=zhihu.com.591380ddf13d4e32969aafc159a80535.797902; zatb=zhihu.com; __utma=51854390.42272649.1392359043.1392477288.1392531585.8; __utmb=51854390.50.9.1392535898367; __utmc=51854390; __utmz=51854390.1392128487.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmv=51854390.100-1|2=registration_date=20120225=1^3=entry_date=20120225=1';
-
+var mycookie = '';
 
 // getQuestion >> getAgree >> getGender;
 var http = require('http');
-var getQuestion = function(questionID){
-    
+var result = [];
+var Counter = 0;
+var getQuestion = function (questionID) {
+    console.time('running time');
 
     var option = {
-        host : 'www.zhihu.com',
-        path : '/question/'+questionID,
-        headers : {
-            Cookie : mycookie
+        host: 'www.zhihu.com',
+        path: '/question/' + questionID,
+        headers: {
+            Cookie: mycookie
         }
-    }
+    };
 
-    var req = http.request(option, function(res){
+    var req = http.request(option, function (res) {
         var data = '';
         res.setEncoding('utf8');
-        res.on('data', function(chunk){
+        res.on('data', function (chunk) {
             data += chunk;
         });
-        res.on('end', function(){
+        res.on('end', function () {
             var arrAns = data.match(/data-aid(.|\n|\r|\t)*?data-score="\d*\.\d*"/g); //正则回答
             var arrAuth = data.match(/<h3 class="zm-item-answer-author-wrap"(.|\n|\r|\t)*?<\/h3>/g); //正则作者
+            Counter = arrAns.length;
 
-            for (var i=0,l=arrAns.length; i<l; i++){
+            console.log('loading...');
+
+            for (var i = 0, l = arrAns.length; i < l; i++) {
                 var id = getAnswerID(arrAns[i]);
+                var score = getScore(arrAns[i]).toFixed(3);
+
                 var author;
-                if (arrAuth[i].indexOf('href="/people/')>=0) {
+                if (arrAuth[i].indexOf('href="/people/') >= 0) {
                     // 获得回答作者名字
-                    author = arrAuth[i].match(/href="\/people.*?<\/a>/)[0].replace(/href="\/people.*?">|<\/a>/g,'');
+                    author = arrAuth[i].match(/href="\/people.*?<\/a>/)[0].replace(/href="\/people.*?">|<\/a>/g, '');
                 } else {
                     author = '匿名用户';
                 }
-                getAgree(id,author);
+
+                new CreateSingleStat(id, author, score);
             }
 
         });
     });
 
-    req.end();    
-}(22774479)
+    req.end();
+}(22836367);
 
 
-var getAnswerID = function(str){
-    // console.log(str.match(/\d+/)[0])
+var getAnswerID = function (str) {
     return str.match(/\d+/)[0];
 };
+var getScore = function (str) {
+    return str.match(/data-score="\d*\.\d*"/)[0].replace(/data-score="|"/g, '') - 0;
+};
 
-var CreatSingleStat = function(answerAuthor){
+var CreateSingleStat = function (answerID, answerAuthor, answerScore) {
     // 单个回答对象
+    this.ID = answerID;
     this.Male = 0;
     this.Female = 0;
     this.reqCount = 0;
     this.Anonymous = 0;
     this.Author = answerAuthor;
+    this.Score = answerScore;
+    this.getAgree = getAgree;
+    this.getGender = getGender;
+    this.getAgree();
 };
 
-var getAgree = function(answerID, answerAuthor){
+var getAgree = function () {
+    var self = this;
     // 取得赞同者
-    var obj = new CreatSingleStat(answerAuthor);
     var option = {
-        host : 'www.zhihu.com',
-        path : '/node/AnswerFullVoteInfoV2?params=%7B%22answer_id%22%3A%22'+answerID+'%22%7D',
-        headers : {
-            Cookie : mycookie
+        host: 'www.zhihu.com',
+        path: '/node/AnswerFullVoteInfoV2?params=%7B%22answer_id%22%3A%22' + self.ID + '%22%7D',
+        headers: {
+            Cookie: mycookie
         }
-    }
-    var req = http.request(option, function(res){
-        var data = '';
-        // res.setEncoding('utf8');
-        res.on('data', function(chunk){
-            data += chunk;
-        });
-        res.on('end', function(){
-            var arr = data.match(/href=".*?"/g);
-            var noname = data.match('匿名用户');
-            if (!data || !arr) {
-                //没有人赞同的回答
-                // console.log('no agree');
-                return ;
-            }
-            if (!!noname)
-                obj.Anonymous = noname.length;
-
-            obj.reqCount = arr.length;
-            for (var i=0,l=arr.length; i<l; i++){
-                getGender(arr[i].replace(/href="\/people\/|"/g,''), obj);
-            }
-            // console.log(arr);
-        });
-    });
-    req.end();
-};
-
-var getGender = function(name, obj){
-    // 取得赞同者性别
-    var option = {
-        host : 'www.zhihu.com',
-        path : '/node/MemberProfileCardV2?params=%7B%22url_token%22%3A%22'+name+'%22%7D',
-        headers : {
-            Cookie : mycookie
-        }
-    }
-    var req = http.request(option, function(res){
+    };
+    var req = http.request(option, function (res) {
         var data = '';
         res.setEncoding('utf8');
-        res.on('data', function(chunk){
+        res.on('data', function (chunk) {
             data += chunk;
         });
-        res.on('end', function(){
-            obj.reqCount--;
-            if ( data.indexOf('<i class="zg-icon female"></i>') >= 0 ) {
-                obj.Female++;
-            } else {
-                obj.Male++;
+        res.on('end', function () {
+            var arr = data.match(/href=".*?"/g);
+            var noname = data.match(/匿名用户/);
+            if (!data || !arr) {
+                //没有人赞同的回答
+                Counter--;
+                return;
             }
-            if (obj.reqCount === 0){
-                delete obj.reqCount;
-                console.log(obj)
+            if (!!noname)
+                self.Anonymous = noname.length;
+
+            self.reqCount = arr.length;
+            for (var i = 0, l = arr.length; i < l; i++) {
+                self.getGender(arr[i].replace(/href="\/people\/|"/g, ''));
             }
         });
     });
     req.end();
-}
+};
+
+var getGender = function (name) {
+    var self = this;
+    // 取得赞同者性别
+    var option = {
+        host: 'www.zhihu.com',
+        path: '/node/MemberProfileCardV2?params=%7B%22url_token%22%3A%22' + name + '%22%7D',
+        headers: {
+            Cookie: mycookie
+        }
+    };
+    var req = http.request(option, function (res) {
+        var data = '';
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function () {
+            self.reqCount--;
+            if (data.indexOf('<i class="zg-icon female"></i>') >= 0) {
+                self.Female++;
+            } else {
+                self.Male++;
+            }
+            if (self.reqCount === 0) {
+                Counter--;
+                delete self.reqCount;
+                delete self.getAgree;
+                delete self.getGender;
+                self.FemalPercent = (self.Female / (self.Male + self.Female + self.Anonymous)).toFixed(3);
+
+                result.push(self);
+                if(Counter == 0){
+                    //TODO
+                    console.timeEnd('running time');
+                }
+            }
+        });
+    });
+    req.end();
+};
